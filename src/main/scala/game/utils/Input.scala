87d005001c5a73d8dac98fd5dbc9295f1d6f1865
Julia Sizon <j.sizon@aimprosoft.com>
2019-02-05 17:32:17 +0200

    More docs, comments

--- src/main/scala/game/utils/Input.scala
+++ src/main/scala/game/utils/Input.scala
@@ -4,7 +4,9 @@ import game.utils.Rules._
 
 import scala.util.matching.Regex
 
-// types to represent input
+/**
+ * Types to represent input
+ **/
 sealed trait Input
 
 case object About extends Input
@@ -30,21 +32,34 @@ case object UnknownCommand extends Input
  **/
 object InputMatcher {
 
-  // convenience method to get input from command line
+  /**
+   * Convenience method to get input from command line
+   *
+   * @return String - input string
+   **/
   def getInput: String = scala.io.StdIn.readLine()
 
-  // pattern to match user input for setting number of players
-  val Pattern: Regex = s"([$PLAYERS_MIN-$PLAYERS_MAX])".r
+  /**
+   * Pattern to match user input for setting number of players
+   *
+   * @return Regex - regex, that considers rules about players amount to match the input on
+   **/
+  private val Pattern: Regex = s"([$PLAYERS_MIN-$PLAYERS_MAX])".r
 
+  /**
+   * Util method for matching the input with case objects, that represent it
+   *
+   * @return Input -representing the input type
+   **/
   def getType(input: String, condition: Option[String] = None): Input = (input, condition) match {
     case (ABOUT, None) => About
     case (PLAY, None) => Play
     case (EXIT, _) => Exit
     case (RESTART, _) => Restart
     case (SPACE, None) => Space
-    case (_, Some(c)) if condition.contains(c) && input == s"$MOVE $c" => MoveUser
-    case (_, Some(c)) if input == s"$MOVE $c" => BadUser
-    case (Pattern(_), None) => RightPlayersNumber
+    case (_, Some(c)) if condition.contains(c) && input == s"$MOVE $c" => MoveUser // if user called move directive right
+    case (_, Some(c)) if input == s"$MOVE $c" => BadUser // if user misspelled player's name
+    case (Pattern(_), None) => RightPlayersNumber // if user input amount of players that passes the rules
     case _ => UnknownCommand
   }
 
