6fe16b5af893927c2fff7ffa559bdefe21ed4e14
Julia Sizon <j.sizon@aimprosoft.com>
2019-02-05 17:39:25 +0200

    edit comments

--- src/main/scala/game/utils/PlayerRegistrationUtil.scala
+++ src/main/scala/game/utils/PlayerRegistrationUtil.scala
@@ -17,12 +17,12 @@ object PlayerRegistrationUtil {
   def retrieveCount: Int = {
     println("Enter number of players (from 2 to 6): ")
 
-    val count: String = InputMatcher.getInput // get count of players from console
+    val count: String = InputMatcher.getInput
 
-    InputMatcher.getType(count) match { // check what input from console is Int
-      case RightPlayersNumber => count.toInt // if Int - return number for players
+    InputMatcher.getType(count) match {
+      case RightPlayersNumber => count.toInt
       case _ =>
-        println("Wrong input or wrong players count! Try again.") // if not - sent error message to console and call retrieveCount method again for input count of players
+        println("Wrong input or wrong players count! Try again.")
         retrieveCount
     }
   }
@@ -35,8 +35,10 @@ object PlayerRegistrationUtil {
    * @return Map of registered users
    */
   def registerUsers(usersCount: Int, users: Users): Users =
-    if (usersCount == 0) users //if count of users end - return already registered players
-    else registerUsers(usersCount - 1, users ++ registerUser(users)) // if count still not 0 - register new player recursively
+  // if count of users end - return already registered players
+    if (usersCount == 0) users
+    // if count still not 0 - register new player recursively
+    else registerUsers(usersCount - 1, users ++ registerUser(users))
 
   /**
    * Method for receiving new username from console and passing it to be checked for uniqueness
@@ -46,13 +48,16 @@ object PlayerRegistrationUtil {
    */
   def registerUser(users: Users): Users = {
     println("Enter player name: ")
-    val newUser = InputMatcher.getInput //retrieve username form console
+
+    //retrieve username form console
+    val newUser = InputMatcher.getInput
 
     if (newUser.length == 0 || newUser == " ") {
       println("Player name can't be empty")
       registerUser(users)
     } else {
-      checkUsers(users, newUser) // pass new username to check for uniqueness
+      // pass new username to check for uniqueness
+      checkUsers(users, newUser)
     }
   }
 
@@ -67,11 +72,13 @@ object PlayerRegistrationUtil {
 
     // if new username is the same as another one in the Map - request a username again
     if (currentUsers.contains(newUser)) {
-      println(s"User $newUser already in game!")
-      registerUser(currentUsers) //if user already exists in map - call registerUser again for retrieve new username/
+      println(s"User $newUser already in the game!")
+      // if user already exists in map - call registerUser again for retrieve new username
+      registerUser(currentUsers)
     } else {
       println(s"$newUser joined!")
-      Map(newUser -> 0)//if username are unique - put it into map with zero position
+      // if username is unique - put it into map with zero position
+      Map(newUser -> 0)
     }
   }
 
